87cb1c14abd24babc9b95b7543ffe3f8a2381528
sungkihwan <singahu@naver.com>
2022-06-16 15:07:21 +0900

    first

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask
+import numpy as np
+
+from konlpy.tag import Okt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+
+app = Flask(__name__)
+sroberta_model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+okt = Okt()
+
+def mmr(doc_embedding, candidate_embeddings, words, top_n, diversity):
+
+    # 문서와 각 키워드들 간의 유사도가 적혀있는 리스트
+    word_doc_similarity = cosine_similarity(candidate_embeddings, doc_embedding)
+
+    # 각 키워드들 간의 유사도
+    word_similarity = cosine_similarity(candidate_embeddings)
+
+    # 문서와 가장 높은 유사도를 가진 키워드의 인덱스를 추출.
+    # 만약, 2번 문서가 가장 유사도가 높았다면
+    # keywords_idx = [2]
+    keywords_idx = [np.argmax(word_doc_similarity)]
+
+    # 가장 높은 유사도를 가진 키워드의 인덱스를 제외한 문서의 인덱스들
+    # 만약, 2번 문서가 가장 유사도가 높았다면
+    # ==> candidates_idx = [0, 1, 3, 4, 5, 6, 7, 8, 9, 10 ... 중략 ...]
+    candidates_idx = [i for i in range(len(words)) if i != keywords_idx[0]]
+
+    # 최고의 키워드는 이미 추출했으므로 top_n-1번만큼 아래를 반복.
+    # ex) top_n = 5라면, 아래의 loop는 4번 반복됨.
+    for _ in range(top_n - 1):
+        candidate_similarities = word_doc_similarity[candidates_idx, :]
+        target_similarities = np.max(word_similarity[candidates_idx][:, keywords_idx], axis=1)
+
+        # MMR을 계산
+        mmr = (1-diversity) * candidate_similarities - diversity * target_similarities.reshape(-1, 1)
+        mmr_idx = candidates_idx[np.argmax(mmr)]
+
+        # keywords & candidates를 업데이트
+        keywords_idx.append(mmr_idx)
+        candidates_idx.remove(mmr_idx)
+
+    return [words[idx] for idx in keywords_idx]
+
+@app.route('/predict/keyword', methods=['POST'])
+def predict_keyword(sentences):
+    tokenized_doc = okt.pos(sentences)
+    tokenized_nouns = ' '.join([word[0] for word in tokenized_doc if word[1] == 'Noun'])
+
+    n_gram_range = (2, 3)
+
+    count = CountVectorizer(ngram_range=n_gram_range).fit([tokenized_nouns])
+    candidates = count.get_feature_names_out()
+
+    embeddings = sroberta_model.encode([sentences])
+    candidate_embeddings = sroberta_model.encode(candidates)
+
+    top_n = 5
+    distances = cosine_similarity(embeddings, candidate_embeddings)
+    keywords = [candidates[index] for index in distances.argsort()[0][-top_n:]]
+
+    return keywords
+
+
+@app.route('/predict/keyword/diversity', methods=['POST'])
+def predict_keyword_diversity(sentences):
+    tokenized_doc = okt.pos(sentences)
+    tokenized_nouns = ' '.join([word[0] for word in tokenized_doc if word[1] == 'Noun'])
+
+    n_gram_range = (2, 3)
+
+    count = CountVectorizer(ngram_range=n_gram_range).fit([tokenized_nouns])
+    candidates = count.get_feature_names_out()
+
+    embeddings = sroberta_model.encode([sentences])
+    candidate_embeddings = sroberta_model.encode(candidates)
+
+    return mmr(embeddings, candidate_embeddings, candidates, top_n=5, diversity=0.7)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5002, debug=True)
+
